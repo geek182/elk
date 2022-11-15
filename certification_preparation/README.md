95bc442eb599867ccd3a4ee778403c2f2fc32302
Leandro Azevedo <leandro.s.azevedo@gmail.com>
2022-11-15 16:01:13 +0100

    updating script and README

--- certification_preparation/README.md
+++ certification_preparation/README.md
@@ -8,3 +8,22 @@
     - APM
     - Structuring and Processing Data
     - Working with Observability Data
+# Todo
+    - install webserver in the VM, and run a job to keep calling the webserver and creating more logs 
+
+
+# Tasks:
+## HeartBeat:
+    - Configure heartbeat to monitor the elasticsearch server (http/http)
+    - Configure heartbeat to monitor a port service (tcp)
+    - Configure heartbeat to ping a host
+    
+## MetricBeat:
+    - Configure the output for connecting to elasticsearch
+    - Test the connectivity
+    - Enable metricbeat module for webserver (apache/nginx)
+    - Configure the module
+    - Test the module syntax
+
+## Filebeat:
+
--- certification_preparation/infrastructure/container/configure-containers.ps1
+++ certification_preparation/infrastructure/container/configure-containers.ps1
@@ -12,7 +12,13 @@ $PodmanStatus =  podman pod ps --format "{{.Status}}" --filter name=elk
 if ([string]::IsNullOrWhitespace($PodmanStatus)){ # Check if output is null or empty
     Write-Output "ELK not found"
     Write-Output "Start fresh startup"
-    
+    Write-Output "Creating a pod called ELK"
+    podman pod create --name elk -p 9200:9200 -p 9300:9300 -p 5601:5601
+    Write-Output "Adding elastic container to pod ELK"
+    podman run --name es01 -e ELASTIC_PASSWORD="elastic12345" -dt --pod elk docker.elastic.co/elasticsearch/elasticsearch:8.4.3
+    Write-Output "Adding kibana container to pod ELK"
+    podman run --name kibana -dt --pod elk docker.elastic.co/kibana/kibana:8.4.3
+
 }
 elseif ($PodmanStatus -eq "Exited") {
     Write-Output "ELK is not running"
